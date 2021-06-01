7dcdfa5d93b59524856148d9e829a0a2b24c376f
Shilpa <56382652+shilparvind@users.noreply.github.com>
2021-06-01 19:34:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(400, 400);
+  var planets = ["Mercury","Venus","Earth","Mars","Jupiter","Saturn","Uranus", "Neptune"]
+
+}
+
+function draw() {
+  background(220);
+}
